84484e7e964377771ae520663350288bd4ef2787
Gang Tao <gang@timeplus.io>
2022-03-17 16:20:10 -0700

    update playground case

---
+++ examples/streamlit/config.py
@@ -0,0 +1,23 @@
+
+config = {
+    "staging" :{
+        "schema" : "https",
+        "host" : "kafka1.dev.timeplus.io",
+        "port" : "443"
+    },
+    "playground" :{
+        "schema" : "https",
+        "host" : "play.timeplus.com",
+        "port" : "443"
+    },
+    "demo" :{
+        "schema" : "https",
+        "host" : "demo.timeplus.com",
+        "port" : "443"
+    },
+    "local" :{
+        "schema" : "http",
+        "host" : "localhost",
+        "port" : "8000"
+    }
+}
