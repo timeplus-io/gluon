45f14e80164ccedac5df92e66754f51e9fdb2bc3
Gang Tao <gang@timeplus.io>
2023-06-23 15:53:54 -0700

    more sample code for types

--- examples/helloworld/sqlalchemy_types.py
+++ examples/helloworld/sqlalchemy_types.py
@@ -1,5 +1,5 @@
 import os
-from sqlalchemy import create_engine, text, select, MetaData, Table
+from sqlalchemy import create_engine, text
 from sqlalchemy.dialects import registry
 
 registry.register("timeplus", "timeplus.sqlalchemy", "TimeplusDialect")
@@ -12,9 +12,23 @@ workspace = os.environ.get("TIMEPLUS_WORKSPACE") or "tp-demo"
 engine = create_engine(
     f"timeplus://:{api_key}@{api_address}:{port}/{workspace}")
 
-# execute driver sql
+# select basic types
 with engine.connect() as conn:
     result = conn.execute(text(
-        "select 1, 1,0, 'abc' , true, false, now(), 1=2, uuid()"))
+        "select 1, 1,0, 'abc' , true, false, today(), now(), now64(), 1=2, uuid()"))
+    for row in result:
+        print(row)
+
+# select compsite types : array, tuple
+with engine.connect() as conn:
+    result = conn.execute(text(
+        "select [1,2,3], (1, 'abc', 1.2)"))
+    for row in result:
+        print(row)
+
+# select compsite types : map
+with engine.connect() as conn:
+    result = conn.execute(text(
+        "SELECT cast(([1, 2, 3], ['a', 'b', 'c']), 'map(int, string)') AS map"))
     for row in result:
         print(row)
