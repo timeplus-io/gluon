1784893445f66f725f746b04ef49e01e080aa108
KJY1029 <jingyao.kang@timeplus.io>
2023-07-07 18:30:25 -0400

    test code update

--- python/test/test_query.py
+++ python/test/test_query.py
@@ -1,13 +1,11 @@
-import json
-
 from timeplus import Stream, Query
 from timeplus.error import TimeplusAPIError
 
 
 def test_query_with_stream(test_environment, test_stream):
     # Test the Query object
-    query_list_before = Query(env=test_environment).list()
-    count = len(query_list_before)
+    query_list = Query(env=test_environment).list()
+    count = len(query_list)
 
     query = (
         Query(env=test_environment)
@@ -15,21 +13,19 @@ def test_query_with_stream(test_environment, test_stream):
         .create()
     )
 
+    query_list = Query(env=test_environment).list()
+    count1 = len(query_list)
+
+    assert count1 == count + 1
     metadata = query.metadata()
     assert "name" in metadata
     assert "sql" in metadata
+    assert "id" in metadata
 
-    query_list_after = Query(env=test_environment).list()
-    count1 = len(query_list_after)
-
-    assert count1 == count + 1
-
+    query_id = query.id
     query.delete()
     test_stream.delete()
 
-    query_list_final = Query(env=test_environment).list()
-    count2 = len(query_list_final)
-
-    assert count2 == count
-
+    query_list = Query(env=test_environment).list()
+    assert query_id not in [q.id for q in query_list]
 
