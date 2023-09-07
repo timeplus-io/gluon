f83105e890ce5671d03fef2d9af2a32d19d21173
Gang Tao <gang@timeplus.io>
2023-09-06 20:57:52 -0700

    add dashboard/udf list

--- python/timeplus/__init__.py
+++ python/timeplus/__init__.py
@@ -8,6 +8,8 @@ from timeplus.view import View  # noqa: F401
 
 from timeplus.source import Source  # noqa: F401
 from timeplus.sink import Sink  # noqa: F401
+from timeplus.udf import UDF  # noqa: F401
+from timeplus.dashboard import Dashboard  # noqa: F401
 from timeplus.dbapi import connect  # noqa: F401
 
 from timeplus.error import (
---
+++ python/timeplus/udf.py
@@ -0,0 +1,37 @@
+from pprint import pprint
+import swagger_client
+from swagger_client.rest import ApiException
+
+
+class UDF:
+    def __init__(self, env):
+        """
+        Initializes the UDF object with the provided environment.
+
+        Args:
+        env: The environment in which the view operates.
+        """
+        self._env = env
+        self._configuration = self._env._conf()
+        self._api_instance = swagger_client.UDFsV1beta2Api(
+            swagger_client.ApiClient(self._configuration)
+        )
+
+    def list(self):
+        """
+        Fetches a list of all UDFs.
+
+        Returns:
+        List: A list of all UDFs.
+
+        Raises:
+        ApiException: If an error occurs during the API call.
+        """
+        try:
+            list_response = self._api_instance.v1beta2_udfs_get()
+            return list_response
+        except ApiException as e:
+            pprint(
+                "Exception when calling UDFsV1beta2Api->v1beta2_udfs_get: %s\n" % e
+            )
+            raise e
