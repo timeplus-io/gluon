5f98031e13e3d4eeee70c700fec9192dc50c4e90
Gang Tao <gangtao@users.noreply.github.com>
2024-09-01 11:05:13 -0700

    Feature/stream engine support (#97)
    * add engine support
    
    * add mutable stream support

--- python/test/test_stream.py
+++ python/test/test_stream.py
@@ -36,3 +36,43 @@ def test_stream(test_environment):
 
     # Ensure stream doesn't exist anymore
     assert not Stream(env=test_environment).name(name).exist()
+
+
+def test_mutable_stream(test_environment):
+    name = "test_mutable_stream"
+
+    # Create a new stream instance with the given name
+    stream = Stream(env=test_environment).name(name)
+
+    try:
+        stream.delete()
+    except Exception:
+        pass
+
+    # create a new stream
+    stream = (
+        Stream(env=test_environment)
+        .name(name)
+        .column("time", "datetime64(3)")
+        .column("data", "string")
+        .column("key", "string")
+        .engine("MutableStream")
+        .primary_key("key")
+        .create()
+    )
+
+    stream_list = Stream(env=test_environment).list()
+    assert name in [q.name for q in stream_list]
+
+    get_stream = Stream(env=test_environment).name(name).get()
+    assert get_stream.metadata().name == name
+
+    # Check the stream exists
+    assert Stream(env=test_environment).name(name).exist()
+
+    stream.delete()
+    stream_list = Stream(env=test_environment).list()
+    assert name not in [q.name for q in stream_list]
+
+    # Ensure stream doesn't exist anymore
+    assert not Stream(env=test_environment).name(name).exist()
